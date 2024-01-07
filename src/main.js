cda3fef6653673755f86b7423d05cd36c7205dff
Huang, Po-Hsiang <yak30257987@gmail.com>
2024-01-08 00:18:02 +0800

    Home組件-圖片懶加載實現(自定義事件)

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import router from "./router";
 import { getCategory } from "@/apis/testAPI";
 // 引入初始化文件
 import "@/styles/common.scss";
+import { useIntersectionObserver } from "@vueuse/core";
 
 // 測試API
 getCategory().then((res) => {
@@ -19,3 +20,16 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+// 定義全局自定義指令
+app.directive("img-lazy", {
+  mounted(element, binding) {
+    // console.log(element, binding);
+    useIntersectionObserver(element, ([{ isIntersecting }]) => {
+      console.log(isIntersecting);
+      if (isIntersecting) {
+        element.src = binding.value;
+      }
+    });
+  },
+});
