afb1b936bdf1734acacecafdcbbde60ec3a29de4
Huang, Po-Hsiang <yak30257987@gmail.com>
2024-01-07 20:41:09 +0800

    Home組件- HomeNew覆用HomePanel

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,19 @@
 import http from "@/utils/http";
 
+// 輪播圖
 export function getBannerAPI() {
   return http({
     url: "/home/banner",
   });
 }
+
+/**
+ * @description: 獲取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const getNewAPI = () => {
+  return http({
+    url: "/home/new",
+  });
+};
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@ import HomeBanner from "./components/HomeBanner.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeHot from "./components/HomeHot.vue";
 import HomeProduct from "./components/HomeProduct.vue";
-import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <template>
@@ -15,10 +14,4 @@ import HomePanel from "./components/HomePanel.vue";
   <HomeNew />
   <HomeHot />
   <HomeProduct />
-  <HomePanel title="人氣商品" subtitle="買一送一"
-    ><div>我是人氣商品的插槽內容</div></HomePanel
-  >
-  <HomePanel title="出清商品" subtitle="全面七折以下"
-    ><div>我是出清商品的插槽內容</div></HomePanel
-  >
 </template>
