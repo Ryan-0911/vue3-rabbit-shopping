6c6a7384abf61533cf9008471484537e8c956dc1
Huang, Po-Hsiang <yak30257987@gmail.com>
2024-01-07 20:19:40 +0800

    Home組件-HomePanel組件封裝(props+slot)

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from "./components/HomeCategory.vue";
 import HomeBanner from "./components/HomeBanner.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeHot from "./components/HomeHot.vue";
-import homeProduct from "./components/HomeProduct.vue";
+import HomeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <template>
@@ -13,5 +14,11 @@ import homeProduct from "./components/HomeProduct.vue";
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <HomePanel title="人氣商品" subtitle="買一送一"
+    ><div>我是人氣商品的插槽內容</div></HomePanel
+  >
+  <HomePanel title="出清商品" subtitle="全面七折以下"
+    ><div>我是出清商品的插槽內容</div></HomePanel
+  >
 </template>
