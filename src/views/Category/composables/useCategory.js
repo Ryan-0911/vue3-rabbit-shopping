26b866016a761ed5f0af8c63ec501219528fa718
Huang, Po-Hsiang <yak30257987@gmail.com>
2024-01-10 15:49:11 +0800

    二級分類-整體認識、路由配置

--- src/views/Category/composables/useCategory.js
+++ src/views/Category/composables/useCategory.js
@@ -1,6 +1,6 @@
 // 封装分类数据业务相关代码
 import { onMounted, ref } from "vue";
-import { getCategoryAPI } from "@/apis/category";
+import { getTopCategoryAPI } from "@/apis/category";
 import { useRoute } from "vue-router";
 import { onBeforeRouteUpdate } from "vue-router";
 
@@ -9,12 +9,12 @@ export function useCategory() {
   const categoryData = ref({});
   const route = useRoute();
   const getCategory = async (id = route.params.id) => {
-    const res = await getCategoryAPI(id);
+    const res = await getTopCategoryAPI(id);
     categoryData.value = res.result;
   };
   onMounted(() => getCategory());
 
-  // 監聽路由變化，路由變化後更新分類資料
+  // 監聽路由變化，當變化時更新分類資料
   onBeforeRouteUpdate((to) => {
     // 存在问题：使用最新的路由参数请求最新的分类数据
     getCategory(to.params.id);
