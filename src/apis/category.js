5d897c02963c38abeb34e633b5c3112bf04ab13e
Huang, Po-Hsiang <yak30257987@gmail.com>
2024-01-10 23:25:57 +0800

    二級分類-麵包屑導航實現

--- src/apis/category.js
+++ src/apis/category.js
@@ -24,3 +24,17 @@ export function getBannerAPI(params = {}) {
     },
   });
 }
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id
+ * @return {*}
+ */
+export const getCategoryFilterAPI = (id) => {
+  return http({
+    url: "/category/sub/filter",
+    params: {
+      id,
+    },
+  });
+};
--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+import { getCategoryFilterAPI } from "@/apis/category";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+
+const route = useRoute();
+// 获取面包屑导航数据
+const filterData = ref({});
+const getFilterData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id);
+  console.log(res);
+  filterData.value = res.result;
+};
+getFilterData();
+</script>
 
 <template>
   <div class="container">
@@ -6,8 +20,11 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家 </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item
+          :to="{ name: 'category', params: { id: filterData.parentId } }"
+          >{{ filterData.parentName }}
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>{{ filterData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
