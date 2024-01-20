ba06a8abf0475320b90f0f538639ade521a0d628
Huang, Po-Hsiang <yak30257987@gmail.com>
2024-01-21 03:47:32 +0800

    登入-Pinia用戶資料持久化

--- src/main.js
+++ src/main.js
@@ -10,10 +10,15 @@ import "@/styles/common.scss";
 // 引入懶加載插件
 import { lazyLoadPlugin } from "@/directives";
 import { componentPlugin } from "@/components";
+// 引入pinia持久狀態插件
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+// 創建pinia並使用持久狀態插件
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 const app = createApp(App);
-
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 app.use(lazyLoadPlugin);
 app.use(componentPlugin);
