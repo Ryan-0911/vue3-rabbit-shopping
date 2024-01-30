ec62384b4617cb547bd8a35a73f6892dc839592c
Huang, Po-Hsiang <yak30257987@gmail.com>
2024-01-30 15:08:50 +0800

    會員中心-會員資訊渲染

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,5 +1,6 @@
 import request from "@/utils/request";
 
+// 登入
 export function loginAPI({ account, password }) {
   return request({
     url: "/login",
@@ -10,3 +11,13 @@ export function loginAPI({ account, password }) {
     },
   });
 }
+
+// 猜你喜歡
+export const getLikeListAPI = ({ limit = 4 }) => {
+  return request({
+    url: "/goods/relevant",
+    params: {
+      limit,
+    },
+  });
+};
